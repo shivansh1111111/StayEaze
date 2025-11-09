47a0c06e615deb237124bfcfddc8e197fd39dd73
shivansh rahangdale <shivanshrahangdale@gmail.com>
2025-11-09 15:38:17 +0530

    Update the code base

---
+++ MICROSERVICES/api-gateway/src/main/java/com/business/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.business.api_gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Slf4j
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ MICROSERVICES/auth-service/src/main/java/com/business/auth/database/entities/RefreshToken.java
@@ -0,0 +1,42 @@
+package com.business.auth.database.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "token_id")
+    private String tokenId;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "token", nullable = false, unique = true, length = 400)
+    private String token;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
